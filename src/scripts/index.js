bb638f528b9833385c4ca168bc1e146662ef3412
Simon <simonsch.tz@gmail.com>
2020-02-18 10:30:53 +0100

    Write my first own promise

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,7 @@
 import '../styles/main.scss';
 // Import any additional modules you want to include below \/
 
-
 // \/ All of your javascript should go here \/
 
+const wait = ms => new Promise((resolve) => setTimeout(resolve, ms))
+wait(6 * 1000).then(() => alert("6 Seconds").catch(failureCallback));
